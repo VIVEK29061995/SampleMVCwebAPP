0575a51b66ac793c818bcc2f57c609e2c5aef8e5
vivek giri <vivek962323@gmail.com>
2022-07-08 18:57:22 +0530

    COMMIT

---
+++ SampleMVCwebAPP/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVCwebAPP.Models;
+
+namespace SampleMVCwebAPP.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult BookEntry()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BookEntry(Book book)
+        {
+            return View();
+        }
+    }
+}
---
+++ SampleMVCwebAPP/Models/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleMVCwebAPP.Models
+{
+    public class Book
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+
+
+        [Display(Name = "Author Name")]
+        [Required(ErrorMessage = "Author is Required")]
+        [MaxLength(20)]
+        public string AuthorName { get; set; }
+
+
+
+        [Display(Name = "Publisher Name")]
+        [MaxLength(40)]
+        public string Publishername { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Date is Required")]
+        public DateTime DateTime { get; set; }
+
+
+
+        [Display(Name = "Price Of Book")]
+        [Required(ErrorMessage = "Price is Required")]
+        public string price { get; set; }
+    }
+}
